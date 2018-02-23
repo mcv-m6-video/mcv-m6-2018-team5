31e7176c78512ae97b3125f2e0c3df71db5a9610
Lidia Garrucho <lidiaxu.3@gmail.com>
2018-02-23 17:46:05 +0100

    little modifications

--- code/metrics/segmentation_metrics.py
+++ code/metrics/segmentation_metrics.py
@@ -7,12 +7,11 @@ def evaluate(testList, gtList):
     TN = 0
     FP = 0
     FN = 0
-    count = 0
-
-    for name in gtList:
-        img = cv.imread(name, cv.IMREAD_GRAYSCALE)
+    num_images = len(testList)
+    for test_image,gt_image in zip(testList,gtList):
+        img = cv.imread(test_image, cv.IMREAD_GRAYSCALE)
         img = cv.normalize(img, alpha=0, beta=1, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
-        gt_img = cv.imread(gtList[count], cv.IMREAD_GRAYSCALE)
+        gt_img = cv.imread(gt_image, cv.IMREAD_GRAYSCALE)
         gt_img = cv.normalize(gt_img, alpha=0, beta=1, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
         h, w = np.shape(img)
         for i in range(0, h):
@@ -27,7 +26,7 @@ def evaluate(testList, gtList):
                         TN += 1
                     else:
                         FN += 1
-        count += 1
+
     print("TP: "+str(TP))
     print("FP" + str(FP))
     print("TN: "+str(TN))
