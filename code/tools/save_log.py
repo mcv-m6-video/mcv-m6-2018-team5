c4f25913526153d7c048851ace129f9225434499
Lidia Garrucho <lidiaxu.3@gmail.com>
2018-02-23 13:31:53 +0100

    Logger

---
+++ code/tools/save_log.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+# Save the printf to a log file
+class Log(object):
+    def __init__(self, log_file):
+        self.terminal = sys.stdout
+        self.log = open(log_file, "a")  # , 0)
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        pass
