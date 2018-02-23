345c322d348d32cc585b1f04d4c24e8591b064d8
Lidia Garrucho <lidiaxu.3@gmail.com>
2018-02-23 13:32:19 +0100

    Configuration files and parser

---
+++ code/config/highway.py
@@ -0,0 +1,17 @@
+# Dataset
+dataset_name                 = 'highway'                        # Dataset name
+dataset_path                 = 'datasets/highway/input'         # Dataset path
+gt_path                      = 'datasets/highway/groundtruth'   # Groung truth path
+results_AB                   = 'datasets/highway/results_testAB_changedetection'
+
+# Input Images
+image_type                   = 'jpg'        # Input image type
+first_image_to_process       = 'in001201'   # Fist image filename
+nr_images                    = 200
+gt_image_type                = 'png'        # Ground truth image type
+first_gt_to_process          = 'gt001201'   # First ground truth image filename
+
+# Save results
+save_results                 = True
+output_folder                = 'results'    # Output folder to save the results of the test
+save_plots                   = False        # Save the plots to disk
---
+++ code/config/load_configutation.py
@@ -0,0 +1,41 @@
+import imp
+import os
+
+class Configuration():
+    def __init__(self, config_path, test_name):
+
+        self.config_path = config_path
+        self.test_name = test_name
+
+    def load(self):
+
+        # Get Config path
+        print self.config_path
+        config_path = os.path.join(os.getcwd(), os.path.dirname(self.config_path),
+                                   os.path.basename(self.config_path))
+        print 'Config file loaded: '
+        print config_path
+
+        cf = imp.load_source('config', config_path)
+
+        # Save extra parameter
+        cf.config_path = config_path
+        cf.test_name = self.test_name
+
+        cf.dataset_path = os.path.abspath(cf.dataset_path)
+        cf.results_AB = os.path.abspath(cf.results_AB)
+
+
+        if cf.save_results:
+
+            # Output folder
+            cf.output_folder = os.path.join(os.path.abspath(cf.output_folder), cf.test_name)
+
+            if not os.path.exists(cf.output_folder):
+                os.makedirs(cf.output_folder)
+
+            print ('Save results in: ' + cf.output_folder)
+            cf.log_file = os.path.join(cf.output_folder, "logfile.log")
+
+        self.configuration = cf
+        return cf
