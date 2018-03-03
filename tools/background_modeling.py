8be6cb1a66b5b87e4d3bde83f22d93a6f5bb0d7f
LidiaGarrucho <lidiaxu.3@gmail.com>
2018-03-03 13:29:53 +0100

    Process only one foreground image

--- tools/background_modeling.py
+++ tools/background_modeling.py
@@ -4,7 +4,6 @@ import cv2 as cv
 import numpy as np
 import time
 
-
 def single_gaussian_modelling(backList):
     logger = logging.getLogger(__name__)
 
@@ -30,27 +29,23 @@ def single_gaussian_modelling(backList):
 
     return mean, variance
 
-
 def foreground_estimation(img, mean, variance, alpha):
     threshold = alpha * (np.sqrt(variance) + 2)
     img = np.abs(cv.imread(img, cv.IMREAD_GRAYSCALE) - mean)
     foreground = (img >= threshold)
     return foreground
 
-
-def adaptive_foreground_estimation(imageList, mean, variance, alpha, rho):
-    foregrounds = []
+def adaptive_foreground_estimation(img, mean, variance, alpha, rho):
     threshold = alpha * (np.sqrt(variance) + 2)
-    for image in imageList:
-        img = cv.imread(image, cv.IMREAD_GRAYSCALE)
-        img_norm = np.abs(img - mean)
-        fore = (img_norm >= threshold)
-        back = (1-fore)
-        img_background = img*back
-        # The mean for the Background pixels (back) is adapted, the mean for the foreground pixels remains the same
-        mean = (rho * img_background + (1-rho)*mean)*back + mean*(1-back)
-        # The variance for the Background pixels (back) is adapted, the variance for the foreground pixels
-        # remains the same
-        variance = (rho * np.square(img_background - mean) + (1-rho)*(variance))*back + (variance)*(1-back)
-        foregrounds.append(fore)
-    return foregrounds
+    img = cv.imread(img, cv.IMREAD_GRAYSCALE)
+    img_norm = np.abs(img - mean)
+    foreground = (img_norm >= threshold)
+    back = (1-foreground)
+    img_background = img*back
+    # The mean for the Background pixels (back) is adapted, the mean for the foreground pixels remains the same
+    mean = (rho * img_background + (1-rho)*mean)*back + mean*(1-back)
+    # The variance for the Background pixels (back) is adapted, the variance for the foreground pixels remains the same
+    variance = (rho * np.square(img_background - mean) + (1-rho)*(variance))*back + (variance)*(1-back)
+    return foreground
+
+
