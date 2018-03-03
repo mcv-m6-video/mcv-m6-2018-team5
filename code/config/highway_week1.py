154a14aa5926c761be85a891bb929cccf87d3f86
LidiaGarrucho <lidiaxu.3@gmail.com>
2018-03-03 12:30:50 +0100

    Config for week 1

---
+++ code/config/highway_week1.py
@@ -0,0 +1,23 @@
+# Dataset
+dataset_name                 = 'highway'                        # Dataset name
+dataset_path                 = 'datasets/highway/input'         # Dataset path
+gt_path                      = 'datasets/highway/groundtruth'   # Ground truth path
+results_path                 = 'datasets/highway/results_testAB_changedetection'
+
+# Input Images
+nr_images                    = 20
+first_image                  = '001201'   # Fist image filename
+image_type                   = 'jpg'        # Input image type
+gt_image_type                = 'png'        # Ground truth image type
+result_image_type            = 'png'
+
+# Segmentation Metrics
+segmentation_metrics          = True        # Precision, Recall, F1-Score
+temporal_metrics              = True        # TP vs time, F1-score vs time
+desynchronization             = True        # Apply desynchronization
+desynchronization_frames      = [0, 5, 10]  # Nr frames to desynchronize
+
+# Save results
+save_results                 = True        # Save Log file
+output_folder                = 'results'   # Output folder to save the results of the test
+save_plots                   = True        # Save the plots to disk
