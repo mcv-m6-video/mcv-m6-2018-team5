caad1b7b35f08dea7acd4a7a00aa3e9a2b6598ec
Lidia Garrucho <lidiaxu.3@gmail.com>
2018-03-05 16:34:05 +0100

    Add multivariative_gaussian_modelling method and foreground_estimation_color

--- tools/background_modeling.py
+++ tools/background_modeling.py
@@ -30,11 +30,56 @@ def single_gaussian_modelling(back_list):
 
     return mean, variance
 
+def multivariative_gaussian_modelling(back_list, color_space = "RGB"):
+    logger = logging.getLogger(__name__)
+
+    start = time.time()
+
+    back_img = cv.imread(back_list[0])
+    if color_space == "YUV":
+        back_img = cv.cvtColor(back_img, cv.CV_BGR2YCrCb)
+
+    mean = np.zeros((back_img.shape[0], back_img.shape[1],back_img.shape[2]))
+    variance = np.zeros((back_img.shape[0], back_img.shape[1], back_img.shape[2]))
+
+    for back_image in back_list:
+        back_img = cv.imread(back_image, cv.IMREAD_COLOR)
+        if color_space == "YUV":
+            back_img = cv.cvtColor(back_img, cv.CV_BGR2YCrCb)
+        mean[:,:,0] = mean[:,:,0] + back_img[:,:,0]
+        mean[:,:,1] = mean[:,:,1] + back_img[:,:,1]
+        mean[:,:,2] = mean[:,:,2] + back_img[:,:,2]
+
+    mean = mean / len(back_list)
+
+    for back_image in back_list:
+        back_img = cv.imread(back_image, cv.IMREAD_COLOR)
+        if color_space == "YUV":
+            back_img = cv.cvtColor(back_img, cv.CV_BGR2YCrCb)
+        variance[:,:,0] = variance[:,:,0] + np.square(back_img[:,:,0] - mean[:,:,0])
+        variance[:,:,1] = variance[:,:,1] + np.square(back_img[:,:,1] - mean[:,:,1])
+        variance[:,:,2] = variance[:,:,2] + np.square(back_img[:,:,2] - mean[:,:,2])
+
+    variance = variance / len(back_list)
+
+    end = time.time()
+
+    logger.info("Background estimated in {:.2f} s".format(end - start))
+
+    return mean, variance
 
 def foreground_estimation(img, mean, variance, alpha):
     threshold = alpha * (np.sqrt(variance) + 2)
     img = np.abs(cv.imread(img, cv.IMREAD_GRAYSCALE) - mean)
     foreground = (img >= threshold)
+    return foreground
+def foreground_estimation_color(img, mean, variance, alpha):
+    img = cv.imread(img) - mean
+    threshold = np.zeros((img.shape[0], img.shape[1], img.shape[2]))
+    threshold[:,:,0] = alpha[0] * (np.sqrt(variance[:,:,0]) + 2)
+    threshold[:,:,1] = alpha[1] * (np.sqrt(variance[:,:,1]) + 2)
+    threshold[:,:,2] = alpha[2] * (np.sqrt(variance[:,:,2]) + 2)
+    foreground = (img[:,:,0] >= threshold[:,:,0]) & (img[:,:,1] >= threshold[:,:,1]) & (img[:,:,2] >= threshold[:,:,2])
     return foreground
 
 
