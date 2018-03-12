16baf9e847fc41a5290aed5e463a67eb2957fad2
Lidia Garrucho <lidiaxu.3@gmail.com>
2018-03-12 15:21:02 +0100

    Modify config files

--- config/highway_background.py
+++ config/highway_background.py
@@ -19,7 +19,7 @@ modelling_method              = 'adaptive'      # adaptive, non-adaptive
 color_images                  = True            # Use RGB, HSV color channels
 color_space                   = "RGB"           # RGB, HSV
 evaluate_foreground           = True
-evaluate_alpha_range          = [0, 10]     # range of alpha values
+evaluate_alpha_range          = [0, 25]     # range of alpha values
 evaluate_alpha_values         = 50          # number of alpha values to evaluate
 evaluate_rho_range            = [0, 1]      # range of rho values
 evaluate_rho_values           = 20          # number of rho values to evaluate
@@ -28,9 +28,15 @@ plot_back_model               = False
 
 # Foreground Modelling
 four_connectivity             = False
-AUC_area_filtering            = True		 # Plot AUC vs P pixels
-P_pixels_range                = [0, 1500]    # range of P pixels
-P_pixels_values               = 30
+AUC_area_filtering            = False		 # Plot AUC vs P pixels
+P_pixels_range                = [0, 1000]    # range of P pixels
+P_pixels_values               = 25
+
+task_name                     = 'task3'      # else task1, task2
+opening_strel                 = 'rectangle'
+opening_strel_size            = 3
+closing_strel                 = 'rectangle'
+closing_strel_size            = 5
 
 # Save results
 save_results                 = True        # Save Log file
