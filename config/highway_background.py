cd57b400643f561d7dfa50258666c0ca874faa01
Lidia Garrucho <lidiaxu.3@gmail.com>
2018-03-13 14:35:19 +0100

    Configs

--- config/highway_background.py
+++ config/highway_background.py
@@ -19,26 +19,26 @@ modelling_method              = 'adaptive'      # adaptive, non-adaptive
 color_images                  = True            # Use RGB, HSV color channels
 color_space                   = "RGB"           # RGB, HSV
 evaluate_foreground           = True
-evaluate_alpha_range          = [0, 10]     # range of alpha values
-evaluate_alpha_values         = 20          # number of alpha values to evaluate
+evaluate_alpha_range          = [0, 25]     # range of alpha values
+evaluate_alpha_values         = 100         # number of alpha values to evaluate
 evaluate_rho_range            = [0, 1]      # range of rho values
 evaluate_rho_values           = 20          # number of rho values to evaluate
 find_best_parameters          = False
 plot_back_model               = False
 
 # Foreground Modelling
-four_connectivity             = False
+four_connectivity             = True
 AUC_area_filtering            = False		 # Plot AUC vs P pixels
 P_pixels_range                = [0, 1000]    # range of P pixels
 P_pixels_values               = 40
 
 task_name                     = 'task1'      # else task1, task2
-opening_strel                 = 'diagonal'
-opening_strel_size            = 10
+opening_strel                 = 'square'
+opening_strel_size            = 5
 closing_strel                 = 'diamond'
 closing_strel_size            = 10
 area_filtering                = False
-area_filtering_P              = 789
+area_filtering_P              = 105
 
 # Save results
 save_results                 = True        # Save Log file
