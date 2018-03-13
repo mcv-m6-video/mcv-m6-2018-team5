f27651821f18d239217fc4a5eb99b358d5e6c1e4
Lidia Garrucho <lidiaxu.3@gmail.com>
2018-03-13 14:33:49 +0100

    fall config

--- config/fall_background.py
+++ config/fall_background.py
@@ -19,8 +19,8 @@ modelling_method              = 'adaptive'      # adaptive, non-adaptive
 color_images                  = True            # Use RGB, HSV color channels
 color_space                   = "RGB"           # RGB, HSV
 evaluate_foreground           = True
-evaluate_alpha_range          = [0, 20]     # range of alpha values
-evaluate_alpha_values         = 40          # number of alpha values to evaluate
+evaluate_alpha_range          = [0, 25]     # range of alpha values
+evaluate_alpha_values         = 100         # number of alpha values to evaluate
 evaluate_rho_range            = [0, 1]      # range of rho values
 evaluate_rho_values           = 20          # number of rho values to evaluate
 find_best_parameters          = False
@@ -32,13 +32,13 @@ AUC_area_filtering            = False		 # Plot AUC vs P pixels
 P_pixels_range                = [0, 1000]    # range of P pixels
 P_pixels_values               = 40
 
-task_name                     = 'task1'      # else task1, task2
-opening_strel                 = 'diagonal'
-opening_strel_size            = 10
+task_name                     = 'task3'      # else task1, task2
+opening_strel                 = 'square'
+opening_strel_size            = 5
 closing_strel                 = 'diamond'
 closing_strel_size            = 10
 area_filtering                = False
-area_filtering_P              = 789
+area_filtering_P              = 1000
 
 # Save results
 save_results                 = True        # Save Log file
