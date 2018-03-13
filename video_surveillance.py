ad11ef7705cac0d64055c9327f5d7d3b6363d3b7
Anna Martí <annamartiaguilera@gmail.com>
2018-03-13 22:33:41 +0100

    morphological filtering for HIGHWAY SEQUENCE

--- video_surveillance.py
+++ video_surveillance.py
@@ -354,11 +354,15 @@ def foreground_estimation(cf):
                             foreground = foreground_improving.image_opening(foreground, cf.opening_strel, cf.opening_strel_size)
 
                             foreground = foreground_improving.image_closing(foreground, cf.closing_strel, cf.closing_strel_size)
-                        elif  cf.dataset_name == 'fall':
+                        elif cf.dataset_name == 'fall':
                             pass
 
                         elif cf.dataset_name == 'highway':
-                            pass
+                            foreground = foreground_improving.image_opening(foreground, cf.opening_strel,
+                                                                            cf.opening_strel_size)
+                            foreground = foreground_improving.image_closing(foreground, cf.closing_strel,
+                                                                            cf.closing_strel_size)
+
 
                     foreground = np.array(foreground, dtype='uint8')
                     tp_temp, fp_temp, tn_temp, fn_temp = seg_metrics.evaluate_single_image(foreground,
@@ -422,8 +426,10 @@ def foreground_estimation(cf):
                             pass
 
                         elif cf.dataset_name == 'highway':
-                            pass
-
+                            foreground = foreground_improving.image_opening(foreground, cf.opening_strel,
+                                                                            cf.opening_strel_size)
+                            foreground = foreground_improving.image_closing(foreground, cf.closing_strel,
+                                                                            cf.closing_strel_size)
 
                     fore = np.array(foreground, dtype='uint8') * 255
                     image_name = os.path.basename(image)
