47243cc6c889fdd9700da8f982a6fdc11d04e56a
XeniaSalinas <salinasxenia@gmail.com>
2018-03-24 14:12:37 +0100

    kalman filter

--- tools/tracking.py
+++ tools/tracking.py
@@ -1,11 +1,32 @@
 import cv2 as cv
+from KalmanFilter import KalmanFilter
+
+class BoundingBox:
+
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.cent_x = x + w // 2
+        self.cent_y = y + h // 2
 
 def extract_bounding_boxes(img):
 
     image, contours, hierarchy = cv.findContours(img, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-    bouningBoxes = []
+    boundingBoxes = []
     for cnt in contours:
         x, y, w, h = cv.boundingRect(cnt)
-        bouningBoxes.append([x, y, w, h])
+        boundingBoxes.append(BoundingBox(x, y, w, h))
 
-    return bouningBoxes
+    return boundingBoxes
+
+def kalman_filter_tracking(imgList):
+
+    bbList = extract_bounding_boxes(imgList[0])
+    kalman_filter_list = []
+    for bb in bbList:
+        kalman_filter_list.append(KalmanFilter([bb.cent_x, bb.cent_y]))
+
+    for img in imgList:
+        bbList = extract_bounding_boxes(img)
